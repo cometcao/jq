2cc3f53117494328c5c26f37eaae401465c94ef2
MetalInvest <MetalInvest@DESKTOP-IARABV4>
2017-08-04 00:32:49 +1000

    fix bugs on kbar

--- JoinQuantStrat/Utility/kBarProcessor.py
+++ JoinQuantStrat/Utility/kBarProcessor.py
@@ -4,10 +4,12 @@ Created on 1 Aug 2017
 
 @author: MetalInvest
 '''
-from kuanke.user_space_api import *
+# from kuanke.user_space_api import *
 from jqdata import *
+import numpy as np
 import copy
 from drawKChart import drawKChart
+from enum import Enum 
 
 class InclusionType(Enum):
     # output: 0 = no inclusion, 1 = first contains second, 2 second contains first
@@ -26,9 +28,9 @@ class KBarProcessor(object):
         '''
         self.kDataFrame_origin = kDf
         self.kDataFrame_modified = copy.deepcopy(kDf)
-        self.kDataFrame_modified.assign(new_high=np.nan, new_low=np.nan, trend_type=np.nan)
+        self.kDataFrame_modified = self.kDataFrame_modified.assign(new_high=np.nan, new_low=np.nan, trend_type=np.nan)
     
-    def isInclusive(self, first, second):
+    def checkInclusive(self, first, second):
         # output: 0 = no inclusion, 1 = first contains second, 2 second contains first
         isInclusion = InclusionType.noInclusion
         if first.high <= second.high and first.low >= second.low:
@@ -51,36 +53,45 @@ class KBarProcessor(object):
         while self.kDataFrame_modified.shape[0] > 2:
             firstElem = self.kDataFrame_modified.iloc[0]
             secondElem = self.kDataFrame_modified.iloc[1]
-            if self.isInclusive(firstElem, secondElem):
-                self.kDataFrame_modified.drop(df.index[0], inplace=True)
+            if self.checkInclusive(firstElem, secondElem) != InclusionType.noInclusion:
+                self.kDataFrame_modified.drop(self.kDataFrame_modified.index[0], inplace=True)
                 pass
             else:
-                self.kDataFrame_modified.iloc[0,'new_high'] = firstElem.high
-                self.kDataFrame_modified.iloc[0,'new_low'] = firstElem.low
+                self.kDataFrame_modified.ix[0,'new_high'] = firstElem.high
+                self.kDataFrame_modified.ix[0,'new_low'] = firstElem.low
                 break
             
         # 2. loop through the whole data set and process inclusive relationship
-        for idx in xrange(self.kDataFrame_modified.shape[0]-3):
+        for idx in xrange(self.kDataFrame_modified.shape[0]-2):
             currentElem = self.kDataFrame_modified.iloc[idx]
-            firstElem = self.kDataFrame_modified.iloc[idx+1]
-            secondElem = self.kDataFrame_modified.iloc[idx+2]
-            if self.isInclusive(firstElem, secondElem):
-                trend = self.kDataFrame_modified.iloc[idx+1,'trend_type'] if not np.isnan(self.kDataFrame_modified.iloc[idx+1,'trend_type']) else self.isBullType(firstElem, secondElem)
+            firstElem = self.kDataFrame_modified.ix[idx+1]
+            secondElem = self.kDataFrame_modified.ix[idx+2]
+            if self.checkInclusive(firstElem, secondElem) != InclusionType.noInclusion:
+                trend = self.kDataFrame_modified.ix[idx+1,'trend_type'] if not np.isnan(self.kDataFrame_modified.ix[idx+1,'trend_type']) else self.isBullType(currentElem, firstElem)
                 if trend:
-                    self.kDataFrame_modified.iloc[idx+2,'new_high']=max(firstElem.high, secondElem.high)
-                    self.kDataFrame_modified.iloc[idx+2,'new_low']=max(firstElem.low, secondElem.low)
+                    self.kDataFrame_modified.ix[idx+2,'new_high']=max(firstElem.high, secondElem.high)
+                    self.kDataFrame_modified.ix[idx+2,'new_low']=max(firstElem.low, secondElem.low)
                     pass
                 else: 
-                    self.kDataFrame_modified.iloc[idx+2,'new_high']=min(firstElem.high, secondElem.high)
-                    self.kDataFrame_modified.iloc[idx+2,'new_low']=min(firstElem.low, secondElem.low)
+                    self.kDataFrame_modified.ix[idx+2,'new_high']=min(firstElem.high, secondElem.high)
+                    self.kDataFrame_modified.ix[idx+2,'new_low']=min(firstElem.low, secondElem.low)
                     pass
-                self.kDataFrame_modified.iloc[idx+2,'trend_type']=trend
-                self.kDataFrame_modified.iloc[idx+1,'new_high']=np.nan
-                self.kDataFrame_modified.iloc[idx+1,'new_low']=np.nan
+                self.kDataFrame_modified.ix[idx+2,'trend_type']=trend
+                self.kDataFrame_modified.ix[idx+1,'new_high']=np.nan
+                self.kDataFrame_modified.ix[idx+1,'new_low']=np.nan
             else:
-                self.kDataFrame_modified.iloc[idx+1,'new_high'] = firstElem.high
-                self.kDataFrame_modified.iloc[idx+1,'new_low'] = firstElem.low
-        pass
+                self.kDataFrame_modified.ix[idx+1,'new_high'] = firstElem.high
+                self.kDataFrame_modified.ix[idx+1,'new_low'] = firstElem.low
+                self.kDataFrame_modified.ix[idx+2,'new_high'] = secondElem.high
+                self.kDataFrame_modified.ix[idx+2,'new_low'] = secondElem.low
+        
+        self.kDataFrame_modified['high'] = self.kDataFrame_modified['new_high']
+        self.kDataFrame_modified['low'] = self.kDataFrame_modified['new_low']
+        self.kDataFrame_modified = self.kDataFrame_modified[np.isfinite(self.kDataFrame_modified['high'])]
+        self.kDataFrame_modified = self.kDataFrame_modified.drop('new_high', 1)
+        self.kDataFrame_modified = self.kDataFrame_modified.drop('new_low', 1)
+        self.kDataFrame_modified = self.kDataFrame_modified.drop('trend_type', 1)
+        return self.kDataFrame_modified
     
     def markTopBot(self):
         
