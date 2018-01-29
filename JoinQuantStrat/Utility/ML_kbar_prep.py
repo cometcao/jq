c5107371c86308696865f86f6f557f5378aaa04b
MetalInvest <MetalInvest@DESKTOP-IARABV4>
2018-01-29 17:42:17 +1100

    completed raw training/testing data

--- JoinQuantStrat/Utility/ML_kbar_prep.py
+++ JoinQuantStrat/Utility/ML_kbar_prep.py
@@ -1,5 +1,7 @@
 from kBarProcessor import *
 from jqdata import *
+import pandas as pd
+import numpy as np
 import talib
 
 class MLKbarPrep(object):
@@ -11,25 +13,26 @@ class MLKbarPrep(object):
     '''
 
     monitor_level = ['1d', '30m']
-    def __init__(self, params):
-        self.isAnal = params.get('isAnal', False)
-        self.count = params.get('count', 100)
+    def __init__(self, count=100, isAnal=False):
+        self.isAnal = isAnal
+        self.count = count
         self.stock_df_dict = {}
     
     def retrieve_stock_data(self, stock):
         for level in MLKbarPrep.monitor_level:
+            local_count = self.count if level == '1d' else self.count * 8
             stock_df = None
-            if self.isAnal:
-                stock_df = attribute_history(stock, self.count, level, fields = ['open','close','high','low', 'money'], skip_paused=True, df=True)  
+            if not self.isAnal:
+                stock_df = attribute_history(stock, local_count, level, fields = ['open','close','high','low', 'money'], skip_paused=True, df=True)  
             else:
                 latest_trading_day = get_trade_days(count=1)[-1]
-                stock_df = get_price(stock, count=self.count, end_date=latest_trading_day, frequency=level, fields = ['open','close','high','low', 'money'], skip_paused=True)          
+                stock_df = get_price(stock, count=local_count, end_date=latest_trading_day, frequency=level, fields = ['open','close','high','low', 'money'], skip_paused=True)          
             stock_df = self.prepare_df_data(stock_df)
             self.stock_df_dict[level] = stock_df
     
     def prepare_df_data(self, stock_df):
         # MACD
-        stock_df.loc[:,'macd_raw'], _, stock_df.loc[:,'macd']  = talib.MACD(stock_df['close'])
+        stock_df.loc[:,'macd_raw'], _, stock_df.loc[:,'macd']  = talib.MACD(stock_df['close'].values)
         # BiaoLi
         stock_df = self.prepare_biaoli(stock_df)
         return stock_df
@@ -38,9 +41,27 @@ class MLKbarPrep(object):
     def prepare_biaoli(self, stock_df):
         kb = KBarProcessor(stock_df)
         kb_marked = kb.getMarkedBL()
-        stock_df = pd.merge(stock_df, kb_marked, on=kb_marked.index, how='outer')
+        stock_df = stock_df.join(kb_marked[['new_index', 'tb']])
+        if self.isAnal:
+            print stock_df
         return stock_df
     
     def prepare_training_data(self):
         higher_df = self.stock_df_dict[MLKbarPrep.monitor_level[0]]
         lower_df = self.stock_df_dict[MLKbarPrep.monitor_level[1]]
+        high_df_tb = higher_df.dropna(subset=['new_index'])
+        if self.isAnal:
+            print high_df_tb
+        high_dates = high_df_tb.index
+        for i in range(0, len(high_dates)-1):
+            first_date = high_dates[i]
+            second_date = high_dates[i+1]
+            trunk_lower_df = lower_df.loc[first_date:second_date,:]
+            if self.isAnal:
+                print trunk_lower_df
+            self.create_ml_data_set(trunk_lower_df, high_df_tb.ix[i, 'tb'])
+        
+    def create_ml_data_set(self, trunk_df, label):
+        pass
+            
+        
