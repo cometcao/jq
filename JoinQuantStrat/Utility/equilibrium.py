923eef2dd301d4977937210857a92569e9d161ac
cometcao <cometcao@gmail.com>
2020-02-11 12:31:27 +0800

    empty data case

--- JoinQuantStrat/Utility/equilibrium.py
+++ JoinQuantStrat/Utility/equilibrium.py
@@ -831,9 +831,11 @@ class NestedInterval():
                                  fields= ['open',  'high', 'low','close'],
                                  skip_paused=True)
                 
-#                 stock_df = attribute_history(self.stock, count=self.count, unit=pe,fields= ['open',  'high', 'low','close'],skip_paused=True)
             kb_df = KBarProcessor(stock_df, isdebug=self.isdebug)
             xd_df = kb_df.getIntegradedXD()
+            if xd_df.empty:
+                self.df_zoushi_tuple_list[pe]=(xd_df,None)
+            else:
             crp_df = CentralRegionProcess(xd_df, isdebug=self.isdebug, use_xd=self.use_xd)
             anal_zoushi = crp_df.define_central_region(initial_direction=initial_direction)
             self.df_zoushi_tuple_list[pe]=(xd_df,anal_zoushi)
