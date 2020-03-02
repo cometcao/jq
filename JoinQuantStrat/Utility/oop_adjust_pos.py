21fa28dcbe532c7d788c9ba1305be2677bb0e029
cometcao <cometcao@gmail.com>
2020-03-02 14:39:04 +0800

    interface bug

--- JoinQuantStrat/Utility/oop_adjust_pos.py
+++ JoinQuantStrat/Utility/oop_adjust_pos.py
@@ -904,15 +904,16 @@ class Short_Chan(Sell_stocks):
             if stock_data.loc[position_time:, 'high'].max() >= top_chan_p: # reached target price
                 print("STOP PROFIT {0} reached target price: {1}".format(stock, top_chan_p))
                 bi_exhausted, bi_xd_exhausted, _= check_sub_chan(stock,
-                                                          end_time=context.current_dt, 
-                                                          period='1m', 
-                                                          direction=TopBotType.bot2top,
-                                                          isdebug=self.isdebug, 
-                                                          is_anal=False,
-                                                          split_time=effective_time,
-                                                          not_check_bi_exhaustion=True,
-                                                          force_zhongshu=True
-                                                          )
+                                                                  end_time=context.current_dt, 
+                                                                  periods=['1m'], 
+                                                                  direction=TopBotType.bot2top,
+                                                                  chan_type=[Chan_Type.INVALID, Chan_Type.I],
+                                                                  isdebug=self.isdebug, 
+                                                                  is_anal=False,
+                                                                  split_time=effective_time,
+                                                                  not_check_bi_exhaustion=True,
+                                                                  force_zhongshu=True
+                                                                  )
                 if bi_exhausted or bi_xd_exhausted:
                     print("STOP PROFIT {0} exhausted: {1}, {2}".format(stock,
                                                                        bi_exhausted,
