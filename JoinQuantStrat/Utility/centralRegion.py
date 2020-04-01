f4fdb1e03243540f42913e105e4d8ffd84f8c91c
cometcao <cometcao@gmail.com>
2020-04-01 20:16:43 +0800

    fix zhongshu exhaustion macd only used for new high/low

--- JoinQuantStrat/Utility/centralRegion.py
+++ JoinQuantStrat/Utility/centralRegion.py
@@ -490,16 +490,20 @@ class ZhongShu(ZouShiLeiXing):
         else:
             first_xd = self.take_first_xd_as_zslx()
             
-        # also need to check balance structure
-        core_region = self.get_core_region()
-        if first_xd.direction == TopBotType.top2bot == last_xd.direction:
-            exhausted = first_xd.zoushi_nodes[0].chan_price > core_region[1] and last_xd.zoushi_nodes[-1].chan_price < core_region[0]
-        elif first_xd.direction == TopBotType.bot2top == last_xd.direction:
-            exhausted = first_xd.zoushi_nodes[0].chan_price < core_region[0] and last_xd.zoushi_nodes[-1].chan_price > core_region[1]
         # check exhaustion
-        if exhausted:
-            exhausted = abs(first_xd.work_out_slope()) > abs(last_xd.work_out_slope()) or\
-                        abs(first_xd.zoushi_nodes[1].macd_acc) > abs(last_xd.zoushi_nodes[1].macd_acc)
+        exhausted = abs(first_xd.work_out_slope()) > abs(last_xd.work_out_slope()) or\
+            
+        if not exhausted:
+            # also need to check balance structure
+            core_region = self.get_core_region()
+            if first_xd.direction == TopBotType.top2bot == last_xd.direction:
+                exhausted = first_xd.zoushi_nodes[0].chan_price > core_region[1] and\
+                            last_xd.zoushi_nodes[-1].chan_price < core_region[0] and\
+                            abs(first_xd.zoushi_nodes[1].macd_acc) > abs(last_xd.zoushi_nodes[1].macd_acc)
+            elif first_xd.direction == TopBotType.bot2top == last_xd.direction:
+                exhausted = first_xd.zoushi_nodes[0].chan_price < core_region[0] and\
+                            last_xd.zoushi_nodes[-1].chan_price > core_region[1] and\
+                            abs(first_xd.zoushi_nodes[1].macd_acc) > abs(last_xd.zoushi_nodes[1].macd_acc)
         return exhausted, last_xd.zoushi_nodes[0].time if exhausted else first_xd.zoushi_nodes[0].time
 
     def is_running_type(self):
