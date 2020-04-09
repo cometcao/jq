788d7fbff15ff36d673769339dae479b98adb72a
cometcao <cometcao@gmail.com>
2020-04-09 10:55:39 +0800

    fix syntax bug

--- JoinQuantStrat/Utility/oop_select_stock.py
+++ JoinQuantStrat/Utility/oop_select_stock.py
@@ -689,7 +689,7 @@ class Filter_Chan_Stocks(Filter_stock_list):
                                 fq_ref_date=context.current_dt.date(), 
                                 df=False)
             min_low = stock_data['low'].min()
-            np.where(stock_data['low']==min_low)[0][-1]
+            start_time = stock_data[np.where(stock_data['low']==min_low)[0][-1]]['date']
             print("start_time: {0}".format(start_time))
             result, profile, _ = check_stock_full(stock,
                                                  end_time=start_time,
