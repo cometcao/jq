5e81d889f17563cae127407a6d34c482a9b7da35
cometcao <cometcao@gmail.com>
2020-04-09 14:58:57 +0800

    add msg

--- JoinQuantStrat/Utility/oop_select_stock.py
+++ JoinQuantStrat/Utility/oop_select_stock.py
@@ -972,7 +972,7 @@ class Filter_common(Filter_stock_list):
         except Exception as e:
             self.log.error(str(e))
         
-        self.log.info("选股过滤（显示前五）:\n{0}".format(join_list(["[%s]" % (show_stock(x)) for x in stock_list[:5]], ' ', 10)))
+        self.log.info("选股过滤（显示前五）:\n{0}, total: {1}".format(join_list(["[%s]" % (show_stock(x)) for x in stock_list[:5]], ' ', 10), len(stock_list)))
         return stock_list
 
     #获取解禁股列表
