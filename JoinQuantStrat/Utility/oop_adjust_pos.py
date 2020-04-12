fa36d81c1d44d81c78cdc83e57382f2854f01cdd
cometcao <cometcao@gmail.com>
2020-04-12 11:09:08 +0800

    revert back for stoploss we don't need to check bi level there

--- JoinQuantStrat/Utility/oop_adjust_pos.py
+++ JoinQuantStrat/Utility/oop_adjust_pos.py
@@ -857,9 +857,8 @@ class Short_Chan(Sell_stocks):
                                                               isdebug=self.isdebug,
                                                               is_anal=False,
                                                               split_time=splitTime,
-                                                              check_bi=True,
-                                                              force_zhongshu=True, 
-                                                              force_bi_zhongshu=True) # synch since we pick min
+                                                              check_bi=False,
+                                                              force_zhongshu=True) # synch with selection
                 if not exhausted or not xd_exhausted:
                     print("sub long point broken")
                     return True
