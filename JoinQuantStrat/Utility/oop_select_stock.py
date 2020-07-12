905de2c42e6f61c5a7d04680e73f174db360f344
MLInvestor <MLInvestor@DESKTOP-FMOI76C>
2020-07-13 00:43:32 +0800

    change vol logic

--- JoinQuantStrat/Utility/oop_select_stock.py
+++ JoinQuantStrat/Utility/oop_select_stock.py
@@ -824,7 +824,7 @@ class Filter_Chan_Stocks(Filter_stock_list):
                                                                            sub_latest_money))
 
         if float_less_equal(cur_latest_money / cur_past_money, 0.809) and\
-            float_less_equal(sub_latest_money / sub_past_money, 0.809):
+            float_more_equal(sub_latest_money / sub_past_money, 1.809):
             self.log.info("candiate stock {0} cur money active: {1} -> {2}, sub money active: {3} -> {4}".format(stock, 
                                                                                cur_past_money, 
                                                                                cur_latest_money,
