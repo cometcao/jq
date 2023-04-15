a8861337b68cf0ea4e6a7b12e44ec94ca874e1bc
cometcao <cometcao@gmail.com>
2023-04-15 16:23:39 +0800

    1 refactor 2 net main money inflow * money traded / cir_mcap / periods

--- JoinQuantStrat/Utility/strat_common_include.py
+++ JoinQuantStrat/Utility/strat_common_include.py
@@ -6,6 +6,52 @@ except:
     pass
 import pandas as pd
 
+def get_main_money_inflow_over_circulating_mcap(stock_list, 
+                                                prv_date, 
+                                                period_count, 
+                                                price_change_filter=None,
+                                                adjust_concentrated=False, 
+                                                force_positive_inflow=True,
+                                                is_debug=False):
+    
+    if price_change_filter is not None:
+        # filter by past time price change
+        history_price = get_bars(stock_list, 
+                                count=period_count, 
+                                unit='1d', 
+                                fields='close',
+                                include_now = True,
+                                df=False)
+        # remove nan values so that only stocks with data more than period count remains
+        history_price = {x:y for x,y in history_price.items() if len(y[~np.isnan(y['close'])]) >= period_count}
+        
+        stock_list = [x for x in stock_list 
+                      if x in history_price and (history_price[x]['close'][-1]-history_price[x]['close'][0])/history_price[x]['close'][0] < price_change_filter / 100]
+        if is_debug:
+            print(stock_list[:10], len(stock_list))
+    
+    
+    # circulating mcap
+    cir_mcap = get_cir_mcap(stock_list, prv_date, adjust_concentrated, is_debug)
+    
+    # main money inflow
+    stock_money_data = get_money_flow(security_list=stock_list, 
+                          end_date=prv_date, 
+                          fields=['sec_code','net_amount_main'], 
+                          count=period_count)
+    net_data= stock_money_data.groupby("sec_code")['net_amount_main'].sum()
+    
+    cir_mcap = cir_mcap.merge(net_data.to_frame(), left_on='code', right_on='sec_code')
+    if force_positive_inflow:
+        cir_mcap = cir_mcap[cir_mcap['net_amount_main'] > 0]
+    if is_debug:
+        print(cir_mcap.head(10))
+    if adjust_concentrated:
+        cir_mcap['mfc'] = cir_mcap['net_amount_main']/(cir_mcap['circulating_market_cap'] * (1-cir_mcap['concentrated_ratio']/100/cir_mcap['cir_total']))
+    else:
+        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['circulating_market_cap']
+    return cir_mcap
+
 
 def filter_stocks_by_price_change(stock_list, period_count, price_change_filter, is_debug=False):
     # filter by past time price change history doesn't contain today's data
@@ -53,96 +99,13 @@ def get_stock_downwards_count(stock_list,
         stock_count[stock] = peak_count
     return stock_count
 
-def get_main_money_inflow_over_time_over_circulating_mcap(
-                                                stock_count_dict,
-                                                prv_date, 
-                                                price_change_filter=None,
-                                                adjust_concentrated=False, 
-                                                force_positive_inflow=True,
-                                                is_debug=False):
-    stock_list = list(stock_count_dict.keys())
-    
+def get_cir_mcap(stock_list, 
+                 current_time, 
+                 adjust_concentrated = False,
+                 is_debug=False
+                ):
     # circulating mcap
-    cir_mcap = get_valuation(stock_list, end_date=prv_date, 
-                        count=1, fields=['circulating_market_cap', 'market_cap'])
-    
-    if is_debug:
-        print(cir_mcap.head(10))
-    
-    if adjust_concentrated:
-        cir_mcap['cir_total'] = cir_mcap['circulating_market_cap'] / cir_mcap['market_cap']
-        cir_mcap['concentrated_ratio'] = 0
-        for stock in stock_list:
-            q0=query(finance.STK_SHAREHOLDER_FLOATING_TOP10).filter(
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.code==stock,
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date<=prv_date.strftime('%Y-%m-%d')
-            ).order_by(finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date.desc()).limit(10)
-            latest_date_df = finance.run_query(q0)
-            
-            if latest_date_df.empty:
-                continue
-            
-            q=query(finance.STK_SHAREHOLDER_FLOATING_TOP10).filter(
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.code==stock,
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.sharesnature=='流通A股',
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date==latest_date_df['pub_date'].values[0]
-                ).order_by(finance.STK_SHAREHOLDER_FLOATING_TOP10.share_ratio.desc()).limit(10)
-            top_10_gd=finance.run_query(q)
-            circulating_concentrated_pct = top_10_gd[top_10_gd['share_ratio']>=5]['share_ratio'].sum()
-            cir_mcap.loc[cir_mcap['code'] == stock, 'concentrated_ratio'] = circulating_concentrated_pct
-        if is_debug:
-            print(cir_mcap.head(10))
-    
-    # main money inflow
-    stock_money_data_list = []
-    for stock in stock_list:
-        stock_money_data = get_money_flow(security_list=stock, 
-                              end_date=prv_date, 
-                              fields=['sec_code','net_amount_main'], 
-                              count=stock_count_dict[stock])
-        stock_money_data_list.append(stock_money_data)
-    
-    net_data= pd.concat(stock_money_data_list).groupby("sec_code")['net_amount_main'].sum()
-        
-    cir_mcap = cir_mcap.merge(net_data.to_frame(), left_on='code', right_on='sec_code')
-    cir_mcap['sum_count'] = cir_mcap['code'].map(stock_count_dict)
-    if force_positive_inflow:
-        cir_mcap = cir_mcap[cir_mcap['net_amount_main'] > 0]
-    if is_debug:
-        print(cir_mcap.head(10))
-    if adjust_concentrated:
-        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['sum_count']/(cir_mcap['circulating_market_cap'] * (1-cir_mcap['concentrated_ratio']/100/cir_mcap['cir_total']))
-    else:
-        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['sum_count']/cir_mcap['circulating_market_cap']
-    return cir_mcap
-
-def get_main_money_inflow_over_circulating_mcap(stock_list, 
-                                                prv_date, 
-                                                period_count, 
-                                                price_change_filter=None,
-                                                adjust_concentrated=False, 
-                                                force_positive_inflow=True,
-                                                is_debug=False):
-    
-    if price_change_filter is not None:
-        # filter by past time price change
-        history_price = get_bars(stock_list, 
-                                count=period_count, 
-                                unit='1d', 
-                                fields='close',
-                                include_now = True,
-                                df=False)
-        # remove nan values so that only stocks with data more than period count remains
-        history_price = {x:y for x,y in history_price.items() if len(y[~np.isnan(y['close'])]) >= period_count}
-        
-        stock_list = [x for x in stock_list 
-                      if x in history_price and (history_price[x]['close'][-1]-history_price[x]['close'][0])/history_price[x]['close'][0] < price_change_filter / 100]
-        if is_debug:
-            print(stock_list[:10], len(stock_list))
-    
-    
-    # circulating mcap
-    cir_mcap = get_valuation(stock_list, end_date=prv_date, 
+    cir_mcap = get_valuation(stock_list, end_date=current_time, 
                         count=1, fields=['circulating_market_cap', 'market_cap'])
     # cir_mcap = get_fundamentals(query(
     #         valuation.code,
@@ -162,7 +125,7 @@ def get_main_money_inflow_over_circulating_mcap(stock_list,
         for stock in stock_list:
             q0=query(finance.STK_SHAREHOLDER_FLOATING_TOP10).filter(
                 finance.STK_SHAREHOLDER_FLOATING_TOP10.code==stock,
-                finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date<=prv_date.strftime('%Y-%m-%d')
+                finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date<=current_time.strftime('%Y-%m-%d')
             ).order_by(finance.STK_SHAREHOLDER_FLOATING_TOP10.pub_date.desc()).limit(10)
             latest_date_df = finance.run_query(q0)
             
@@ -179,30 +142,59 @@ def get_main_money_inflow_over_circulating_mcap(stock_list,
             cir_mcap.loc[cir_mcap['code'] == stock, 'concentrated_ratio'] = circulating_concentrated_pct
         if is_debug:
             print(cir_mcap.head(10))
+    return cir_mcap
+
+def get_main_money_inflow(stock_list, prv_date, stock_count_dict):
+    stock_money_data_list = []
+    for stock in stock_list:
+        stock_money_data = get_money_flow(security_list=stock, 
+                              end_date=prv_date, 
+                              fields=['sec_code','net_amount_main'], 
+                              count=stock_count_dict[stock])
+        stock_money_data_list.append(stock_money_data)
+    net_data= pd.concat(stock_money_data_list).groupby("sec_code")['net_amount_main'].sum()
+    return net_data
+
+def get_main_money_inflow_over_time_over_circulating_mcap(
+                                                stock_count_dict,
+                                                prv_date, 
+                                                price_change_filter=None,
+                                                adjust_concentrated=False, 
+                                                force_positive_inflow=True,
+                                                is_debug=False):
+    stock_list = list(stock_count_dict.keys())
+    
+    # circulating mcap
+    cir_mcap = get_cir_mcap(stock_list, prv_date, adjust_concentrated, is_debug)
     
     # main money inflow
-    stock_money_data = get_money_flow(security_list=stock_list, 
-                          end_date=prv_date, 
-                          fields=['sec_code','net_amount_main'], 
-                          count=period_count)
-    net_data= stock_money_data.groupby("sec_code")['net_amount_main'].sum()
+    net_data = get_main_money_inflow(stock_list, prv_date, stock_count_dict)
+        
     cir_mcap = cir_mcap.merge(net_data.to_frame(), left_on='code', right_on='sec_code')
+    cir_mcap['sum_count'] = cir_mcap['code'].map(stock_count_dict)
     if force_positive_inflow:
         cir_mcap = cir_mcap[cir_mcap['net_amount_main'] > 0]
     if is_debug:
         print(cir_mcap.head(10))
     if adjust_concentrated:
-        cir_mcap['mfc'] = cir_mcap['net_amount_main']/(cir_mcap['circulating_market_cap'] * (1-cir_mcap['concentrated_ratio']/100/cir_mcap['cir_total']))
+        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['sum_count']/(cir_mcap['circulating_market_cap'] * (1-cir_mcap['concentrated_ratio']/100/cir_mcap['cir_total']))
     else:
-        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['circulating_market_cap']
+        cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['sum_count']/cir_mcap['circulating_market_cap']
     return cir_mcap
+
+
 
 def get_main_money_inflow_over_total_money_over_time(
                                                 stock_count_dict,
                                                 current_time, 
                                                 force_positive_inflow=True,
+                                                adjust_concentrated=True,
                                                 is_debug=False):
     stock_list = list(stock_count_dict.keys())
+    
+    # circulating mcap
+    cir_mcap = get_cir_mcap(stock_list, current_time, adjust_concentrated, is_debug)
+    cir_mcap = cir_mcap.set_index('code')
     
     # all traded money over the designated period
     stock_money_data = {}
@@ -215,30 +207,26 @@ def get_main_money_inflow_over_total_money_over_time(
                                df = False,
                                include_now=True)
         stock_money_data[stock] = money_data['money'].sum()
-    cir_mcap = pd.DataFrame.from_dict(stock_money_data, orient='index', columns=['money'])    
-    cir_mcap.index.name = 'code'
+    # cir_mcap = pd.DataFrame.from_dict(stock_money_data, orient='index', columns=['money'])    
+    cir_mcap['money'] = pd.Series(stock_money_data)
+    
     if is_debug:
         print(cir_mcap.head(10))
         
     # main money inflow
-    stock_money_data_list = []
-    for stock in stock_list:
-        stock_money_data = get_money_flow(security_list=stock, 
-                              end_date=current_time, 
-                              fields=['sec_code','net_amount_main'], 
-                              count=stock_count_dict[stock])
-        stock_money_data_list.append(stock_money_data)
+    net_data = get_main_money_inflow(stock_list, current_time, stock_count_dict)
     
-    net_data= pd.concat(stock_money_data_list).groupby("sec_code")['net_amount_main'].sum()
-        
-    cir_mcap = cir_mcap.merge(net_data.to_frame(), left_on='code', right_on='sec_code')
-    cir_mcap['sum_count'] = cir_mcap['code'].map(stock_count_dict)
+
+    
+    cir_mcap = cir_mcap.merge(net_data.to_frame(), left_index=True, right_index=True)
+    cir_mcap['sum_count'] = pd.Series(stock_count_dict)
     if force_positive_inflow:
         cir_mcap = cir_mcap[cir_mcap['net_amount_main'] > 0]
     if is_debug:
         print(cir_mcap.head(10))
 
-    cir_mcap['mfc'] = cir_mcap['net_amount_main']/cir_mcap['money']/cir_mcap['sum_count']
+    cir_mcap['mfc'] = cir_mcap['net_amount_main']*cir_mcap['money']/cir_mcap['circulating_market_cap']/cir_mcap['sum_count']
+    cir_mcap.index.name = 'code'
     return cir_mcap
 
 ############################
