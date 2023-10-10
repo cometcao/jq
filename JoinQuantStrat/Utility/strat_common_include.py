6539c5511d6b29df5f78cf2d439bf184a4112426
cometcao <cometcao@gmail.com>
2023-10-10 17:58:40 +0800

    update the analytical piece

--- JoinQuantStrat/Utility/strat_common_include.py
+++ JoinQuantStrat/Utility/strat_common_include.py
@@ -325,6 +325,14 @@ def get_main_money_inflow_over_total_money_over_time(
 #
 # print(cir_mcap[['code', 'day', 'net_amount_main', 'net_amount_main_roll', 'mfc']])
 
+
+
+
+
+
+#############################
+#############################
+#############################
 # # main money inflow over ciculating market cap adjusted by top 10 concentrated ownership
 # from jqdata import *
 # from datetime import datetime, timedelta
@@ -342,54 +350,66 @@ def get_main_money_inflow_over_total_money_over_time(
 # print("current time: {0}".format(current_time))
 # # working_date = datetime.strptime('2022-04-28', "%Y-%m-%d")
 #
-# period_count = 20
-# downwards_count_limit = 10 if period_count == 60 else 5
-# print("period count: {0}, limit: {1}".format(period_count, downwards_count_limit))
+# use_money = True
 # debug = False
-#
 # level_check = '1d'
 # stock_list = get_all_securities(['stock']).index.values.tolist()
 #
-# # I: find a rough downwards movement and worm out the period
-# stock_count = get_stock_downwards_count(stock_list, period_count, current_time, '1d', downwards_count_limit, 0, debug)
+#
+# def filter_out_stock_by_mfc(period_count, current_time, use_money, debug):
+#     downwards_count_limit = 10 if period_count == 60 else 5
+#     print("period count: {0}, limit: {1}, use_money: {2}".format(period_count, downwards_count_limit, use_money))
+#     # I: find a rough downwards movement and worm out the period
+#     stock_count = get_stock_downwards_count(
+#         stock_list, period_count, current_time, '1d', downwards_count_limit, 0, debug)
+#
+#     # II: find positive top money inflow / free share portion ######################################
+#     # check all stocks
+#     # cir_mcap = get_main_money_inflow_over_time_over_circulating_mcap(stock_count, 
+#     #                                                                current_time, 
+#     #                                                                price_change_filter = 0,
+#     #                                                                adjust_concentrated=True, 
+#     #                                                                force_positive_inflow=True,
+#     #                                                                is_debug=debug)
+#     cir_mcap = get_main_money_inflow_over_total_money_over_time(stock_count, 
+#                                                                current_time, 
+#                                                                force_positive_inflow=False,
+#                                                                use_cir_mcap=True,
+#                                                                use_money=use_money,
+#                                                                is_debug=debug)
+#
+#     # remove STI stocks
+#     # cir_mcap = cir_mcap[cir_mcap['code'].map(lambda x: x[:3] != '688')]
+#
+#     cir_mcap = cir_mcap.sort_values(by='mfc', ascending=False)
+#     cir_mcap = cir_mcap.reset_index();
+#
+#     # only consider top 100
+#     stock_value_list = cir_mcap.head(100)[['code', 'mfc']].values.tolist()
+#
+#     print(stock_value_list)
+#     return stock_value_list
+#
+# stock_list_20 = filter_out_stock_by_mfc(20, current_time, use_money, debug)
+# stock_list_60 = filter_out_stock_by_mfc(60, current_time, use_money, debug)
+# stock_list_20 = [stock for stock,_ in stock_list_20]
+# stock_list_60 = [stock for stock,_ in stock_list_60]
+# stock_value_list = [stock for stock in stock_list_20 if stock in stock_list_60]
+#
+# ####################################################################################################
 #
 #
-# # II: find positive top money inflow / free share portion ##############
-# # check all stocks
-# # cir_mcap = get_main_money_inflow_over_time_over_circulating_mcap(stock_count,
-# #                                                                current_time,
-# #                                                                price_change_filter = 0,
-# #                                                                adjust_concentrated=True,
-# #                                                                force_positive_inflow=True,
-# #                                                                is_debug=debug)
-# cir_mcap = get_main_money_inflow_over_total_money_over_time(stock_count,
-#                                                            current_time,
-#                                                            force_positive_inflow=False,
-#                                                            use_cir_mcap=True,
-#                                                            use_money=True,
-#                                                            is_debug=debug)
+# money_inflow_list = stock_value_list
 #
-# # remove STI stocks
-# # cir_mcap = cir_mcap[cir_mcap['code'].map(lambda x: x[:3] != '688')]
-#
-# cir_mcap = cir_mcap.sort_values(by='mfc', ascending=False)
-# cir_mcap = cir_mcap.reset_index();
-#
-# # only consider top 100
-# stock_value_list = cir_mcap.head(100)[['code', 'mfc']].values.tolist()
-#
-# print(stock_value_list)
-# money_inflow_list = [stock for stock, _ in stock_value_list]
-#
-# # III: apply zoushi analysis ###########################################
-# expected_zoushi_down = [ZouShi_Type.Pan_Zheng, ZouShi_Type.Qu_Shi_Down] #
+# # III: apply zoushi analysis ##################################################################
+# expected_zoushi_down = [ZouShi_Type.Pan_Zheng, ZouShi_Type.Qu_Shi_Down] #  
 # expected_exhaustion_down = [Chan_Type.PANBEI, Chan_Type.BEICHI] #Chan_Type.INVIGORATE
 #
 # def get_translated_count(count, level):
 #     if count <= 20 and level == '1d':
 #         return [(count * 4, '60m'), (count * 8, '30m')]
 #     elif count <= 60 and level == '1d':
-#         return [(count, '1d')]
+#         return [(count, '1d')] 
 #     else:
 #         return [(count, level)]
 # picked_stocks = []
@@ -397,11 +417,11 @@ def get_main_money_inflow_over_total_money_over_time(
 #     current_time=pd.datetime.now()
 #
 #     for check_count_level, level in get_translated_count(period_count, '1d'):
-#         stock_data = get_bars(stock,
-#                                count=check_count_level,
-#                                end_dt=current_time,
+#         stock_data = get_bars(stock, 
+#                                count=check_count_level, 
+#                                end_dt=current_time, 
 #                                unit=level,
-#                                fields= ['date','high', 'low'],
+#                                fields= ['date','high', 'low'], 
 #                                df = False,
 #                                include_now=True)
 #
@@ -412,11 +432,11 @@ def get_main_money_inflow_over_total_money_over_time(
 #             continue
 #
 #         result_zoushi, result_exhaustion = analyze_MA_zoushi_by_stock(stock=stock,
-#                                                                   period=level,
+#                                                                   period=level, 
 #                                                                   count=peak_count,
-#                                                                   end_dt=current_time,
-#                                                                   df=False,
-#                                                                   zoushi_types=expected_zoushi_down,
+#                                                                   end_dt=current_time, 
+#                                                                   df=False, 
+#                                                                   zoushi_types=expected_zoushi_down, 
 #                                                                   direction=TopBotType.top2bot)
 #         if result_zoushi in expected_zoushi_down and result_exhaustion in expected_exhaustion_down:
 #             print("stock: {0} zoushi: {1} exhaustion:{2} level:{3}".format(stock, result_zoushi, result_exhaustion, level))
